e23d11dab95dd6dae992bcf51e5d9d2d282ce61f
AdiatsaPutra <santikosetyo77@gmail.com>
2021-07-12 12:04:40 +0700

    Added doa harian page

--- lib/presentation/pages/doa_harian_page.dart
+++ lib/presentation/pages/doa_harian_page.dart
@@ -5,6 +5,7 @@ import 'package:muslim/cubit/doa_harian_cubit.dart';
 import 'package:muslim/models/models.dart';
 import 'package:muslim/presentation/pages/layout/detail_category_page.dart';
 import 'package:muslim/presentation/utils/constant.dart';
+import 'package:shimmer/shimmer.dart';
 
 class DoaHarianPage extends StatelessWidget {
   @override
@@ -12,40 +13,65 @@ class DoaHarianPage extends StatelessWidget {
     return DetailCategoryPage(
       pageTitle: 'Doa Harian',
       text: 'text',
-      child: BlocBuilder<DoaHarianCubit, DoaHarianState>(
-        builder: (context, state) {
-          if (state is DoaHarianLoaded) {
-            List<DoaHarian> doaHarian = state.doaHarian;
-            return SingleChildScrollView(
-              child: ListView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: doaHarian.length,
-                itemBuilder: (context, index) {
-                  return Container(
-                    margin: EdgeInsets.only(
-                      bottom: 10,
-                    ),
-                    padding: EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: shadow,
-                    ),
-                    child: Column(
-                      children: [
-                        Text(doaHarian[index].arabic),
-                        Text(doaHarian[index].title),
-                        Text(doaHarian[index].translation),
-                      ],
-                    ),
-                  );
-                },
-              ),
-            );
-          } else {
-            return CircularProgressIndicator();
+      child: BlocConsumer<DoaHarianCubit, DoaHarianState>(
+        listener: (context, state) {
+          if (state is DoaHarianError) {
+            Get.snackbar('Periksa Koneksi Anda', state.message);
           }
+        },
+        builder: (context, state) {
+          return BlocBuilder<DoaHarianCubit, DoaHarianState>(
+            builder: (context, state) {
+              if (state is DoaHarianLoaded) {
+                List<DoaHarian> doaHarian = state.doaHarian;
+                return SingleChildScrollView(
+                  child: ListView.builder(
+                    physics: NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    itemCount: doaHarian.length,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: EdgeInsets.only(
+                          bottom: 10,
+                        ),
+                        padding: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: shadow,
+                        ),
+                        child: Column(
+                          children: [
+                            Text(doaHarian[index].arabic),
+                            Text(doaHarian[index].title),
+                            Text(doaHarian[index].translation),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                );
+              } else {
+                return ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Shimmer.fromColors(
+                      child: Container(
+                        height: 100,
+                        margin: EdgeInsets.all(20),
+                        decoration: BoxDecoration(
+                          color: Colors.grey,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      baseColor: Colors.grey.withOpacity(0.1),
+                      highlightColor: Colors.white,
+                    );
+                  },
+                );
+              }
+            },
+          );
         },
       ),
       onBack: () {
